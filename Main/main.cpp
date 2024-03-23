f475d1f5dee91fcdec78dd8dac3aa1b6e8868a8c
wilches <dvargasw@unal.edu.co>
2024-03-23 18:31:36 -0500

    se actualizo el makefile con el fin de usar sanitizers en la carpeta Main, ademas de añadir el graficador que realiza la figura del punto 1, y por tanto la finalizacion de este ultimo

--- Main/main.cpp
+++ Main/main.cpp
@@ -8,8 +8,8 @@ int main() {
     //Configuracion de parametros
     const int t_final = 6000000;
     const int N_particles = 400;
-    const double x_min = -10, x_max = 10;
-    const double y_min = -10, y_max = 10;
+    const double x_min = -20, x_max = 20;
+    const double y_min = -20, y_max = 20;
     const int divisions = 8;
     // Inicializa n objetos de la clase Particle y los añade al vector
     inicializar(balls, N_particles);
@@ -18,7 +18,7 @@ int main() {
     for (auto ball : balls){
         counts(grid_counts2, ball.getX(), ball.getY(), x_min, x_max, y_min, y_max, divisions);
     }
-    double entropy2 = compute_entropy(grid_counts2, N_particles);
+    double entropy = compute_entropy(grid_counts2, N_particles);
    
     int coin;
     for (int i = 0; i < t_final; i++)
@@ -28,7 +28,7 @@ int main() {
         // %%%%%%%%%%%%% Posicion antigua %%%%%%%%%%%%%
         double old_x = balls[coin].getX();
         double old_y = balls[coin].getY();
-        balls[coin].moveRandom(0.1, x_min, x_max, y_min, y_max);        
+        balls[coin].moveRandom(0.125, x_min, x_max, y_min, y_max);        
         
         // %%%%%%%%%%%%% Posicion actual %%%%%%%%%%%%%
         double new_x = balls[coin].getX();
@@ -36,9 +36,9 @@ int main() {
         
         // %%%%%%%%%%%%% Calcula el cambio en la entropia (2 casos) %%%%%%%%%%%%%
         double ds = delta_entropy(grid_counts2, new_x, new_y, x_min, x_max, y_min, y_max, divisions, old_x, old_y, N_particles);
-        entropy2 += ds;
+        entropy += ds;
 
-        entropia<<i<<"\t"<<entropy2<<std::endl;
+        entropia<<i<<"\t"<<entropy<<std::endl;
        
         
     }
